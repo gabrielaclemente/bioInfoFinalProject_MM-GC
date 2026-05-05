58815efb4fbb05b901c0c30af9359caa9e195f75
gabrielaclemente <159827348+gabrielaclemente@users.noreply.github.com>
2026-05-05 19:17:51 -0400

    main added with just comments

---
+++ main.py
@@ -0,0 +1,10 @@
+# main 
+
+# we need to figure out the data analysis stuff 
+# we also need to understand what we would like to do for the poster 
+# we want to understand what data would be intresting to show people 
+# essentially we are trying to figure out which genes are affected after TBI, but also have a good idea
+# of the effectiveness of the two different types of algorithms we are using
+
+#here is where we will actually run the files and outpit the results CSVs while also calling the 
+#DA functions which we choose to use
